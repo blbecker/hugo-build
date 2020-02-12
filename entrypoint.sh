726922cec6220c01ade0125fe19386dee0cc2ecd
Benjamin Becker <benjamin@bckr.me>
2020-02-12 10:34:16 -0800

    Print line branch and sha.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,8 @@ set -e
 export GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 export GIT_SHA=$(git rev-parse --verify HEAD --short)
 
+echo -e "GIT_BRANCH=${GIT_BRANCH}\nGIT_SHA=${GIT_SHA}"
+
 git submodule init && git submodule update --recursive
 
 hugo
