233590cf293137aaaa0a6a1e33692760834ff2b8
Benjamin Becker <benjamin@bckr.me>
2020-02-12 10:29:40 -0800

    Add github branch and sha.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+export GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+export GIT_SHA=$(git rev-parse --verify HEAD --short)
+
 git submodule init && git submodule update --recursive
 
 hugo
