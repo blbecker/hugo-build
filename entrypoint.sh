ac1c71ac169439050f0ff4ad0e0d4a34e740e028
Benjamin Becker <benjamin@bckr.me>
2020-02-12 13:59:59 -0800

    Use the github ref var instead of determining branch directly from git.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-export GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+export GIT_BRANCH=$(echo "${GITHUB_REF}" | cut -f 3- -d "/")
 export GIT_SHA=$(git rev-parse --verify HEAD --short)
 
 echo -e "GIT_BRANCH=${GIT_BRANCH}\nGIT_SHA=${GIT_SHA}"
