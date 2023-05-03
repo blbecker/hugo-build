84ae3081d0e01787e2ffcf054692740743f097cf
Benjamin Becker <benjamin.becker@teamnorthwoods.com>
2023-05-03 13:38:26 -0400

    Add a safe directory exception for the github workspace.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# Add actions workspace to safe directories
+git config --global --add safe.directory /github/workspace
+
 git submodule init && git submodule update --recursive
 
 hugo ${INPUT_HUGO_ARGS}
