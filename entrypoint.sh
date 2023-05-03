a57b39be9ca5eb0dd2facad765ef1fd273cdd452
Benjamin Becker <benjamin.becker@teamnorthwoods.com>
2023-05-03 10:20:06 -0400

    Update to hugo version 0.111.3. Drop the git variables, because hugo has that built in now.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-export GIT_BRANCH=$(echo ${GITHUB_REF} | cut -f 3- -d "/")
-export GIT_SHA=$(git rev-parse --verify HEAD --short)
-
-echo -e "GIT_BRANCH=${GIT_BRANCH}\nGIT_SHA=${GIT_SHA}"
-
 git submodule init && git submodule update --recursive
 
 hugo ${INPUT_HUGO_ARGS}
